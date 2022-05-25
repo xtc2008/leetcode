fa4918dc6dd2c7378de13aa5fd52b63b960b50fb
Tao Chen <xtc2008@gmail.com>
2022-05-25 12:05:55 -0400

    longest happy string

--- 1405.longest-happy-string.js
+++ 1405.longest-happy-string.js
@@ -12,37 +12,40 @@
  * @return {string}
  */
 var longestDiverseString = function(a, b, c) {
-    const arr = [["a", a], ["b", b], ["c", c]];
-    arr.sort((a,b) => b[1] - a[1]);
-        
-    const first = Array(arr[0][1]).fill(arr[0][0]);
-    const mid = Array(arr[1][1]).fill(arr[1][0]);
-    const last = Array(arr[2][1]).fill(arr[2][0]);
+    //create counters for each string so we can track when they hit 2
+    let countA = 0;
+	let countB = 0;
+	let countC = 0;
+	var result = "";
+    //if any value is greater than zero we will loop through a, b, c
+	while (a > 0 || b > 0 || c > 0) {
+        //each condition handles 1 letter, first condition will handle a.  If a is greater or equal to b and c or
+        //count of other letters are greater equal to 2 and a is greater than 0, a will go.
+        //Once a letter takes their turn, the counts are reset for the other letters.
+		if ((a >= b && a >= c && countA < 2) || (countB === 2 && a > 0) || (countC === 2 && a > 0)) {
+			result += "a";
+			countA += 1;
+			a -= 1;
+			countB=0;
+			countC=0;
+		} else if ((b >= a && b >= c && countB < 2 ) || (countA === 2 && b > 0) || (countC === 2 && b > 0)) {
+			result += "b";
+			countB += 1;
+			b -= 1;
+			countA=0;
+			countC=0;
+		} else if ((c >= a && c >= b && countC < 2) || (countB === 2 && c > 0) || (countA === 2 && c > 0)) {
+			result += "c";
+			countC += 1;
+			c -= 1;
+			countA=0;
+			countB=0;
+		} else {
+			break;
+		}
+	}
 
-    const fullArray = first.concat(mid).concat(last);
-
-    console.log(fullArray);
-    console.log(fullArray.at(-1))
-
-    let results = "";
-
-    while(fullArray) {
-        // if(fullArray[0] === fullArray[1]) {
-        //     results += fullArray.shift();
-        // } else {
-        //     results += fullArray.shift();
-        // }
-
-        // if(fullArray.at(-1) === fullArray.at(-2)) {
-        //     results += fullArray.pop()
-        // } else {
-        //     results += fullArray.pop()
-        // }
-        results += fullArray.shift();
-        results += fullArray.pop()
-    }
-
-    return results;
+	return result;
 };
 
 console.log(longestDiverseString(1,1,7))
